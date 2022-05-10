bce0b75499435df4872fc744f1c6383dee52c40f
Silje Denise <siljedenise@gmail.com>
2022-05-10 16:38:16 +0200

    fix: Fixed styling for AvatarHeader

--- smidig/src/components/AvatarHeader.jsx
+++ smidig/src/components/AvatarHeader.jsx
@@ -1,20 +1,35 @@
 import React from "react";
 import styled from "styled-components";
-
+//import {FontAwesomeIcon} from '@fortawesome/fontawesome-free';
+//mport {faUser} from '@fortawesome/free-solid-svg-icons';
 
 const StyledAvatarHeader = styled("div")`
-    background-color:red;
-    height: 100px;
+    height: 200px;
     display:flex;
     justify-content:center;
+    align-items:center;
+    flex-direction: column;
+    padding:60px;
+`;
+const StyledAvatarImage = styled("div")`
+    border-radius: 100%;
+    border: solid 2px #ccc;
+    width: 100px;
+    height: 100px;
+`;
+const StyledHeaderText = styled("h3")`
+    @import url("https://fonts.googleapis.com/css2?family=Source+Sans+Pro&display=swap");
+    font-size:29px;
+    font-family: "Source Sans Pro", sans-serif;
 `;
 
 const AvatarHeader = () => {
     return(
         <>
             <StyledAvatarHeader>
-                <img src="../images/avatar.png" alt="avatar picture"/>
-                <h3>Velg din avatar</h3>
+                {/*<FontAwesomeIcon icon={faUser} size={'2x'}/>*/}
+               <StyledAvatarImage></StyledAvatarImage>
+                <StyledHeaderText>Velg din avatar</StyledHeaderText>
             </StyledAvatarHeader>
         </>
     );
