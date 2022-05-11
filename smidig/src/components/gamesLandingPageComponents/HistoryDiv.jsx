bbbcb7700a4d10a121114fc4562272b008acb9df
abc <abc@example.com>
2022-05-11 13:11:56 +0200

    VikingDiv update

---
+++ smidig/src/components/gamesLandingPageComponents/HistoryDiv.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+
+const HistoryDiv = () => {
+
+return(
+    <div>
+        <h3>Historien</h3>
+        <img src="Images/Viking.png" alt="Bilde av en viking" />
+        <p>Følg oss gjennom stien, og hjelp avataren</p>
+    </div>
+);
+
+
+}
+export default HistoryDiv;
