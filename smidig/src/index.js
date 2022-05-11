6cb74860dcdddafa6e0e8198703baac3bced7389
Sandra-png <sandra.e@hotmail.no>
2022-05-11 14:25:29 +0200

    Feat: Added Badges component, and a "Test Page".
    Badges are now working components, to be used in the Profile Page. Since we are working as a group of two, we have added a new page just to visualize the content without merge-conflicts arising.

--- smidig/src/index.js
+++ smidig/src/index.js
@@ -6,6 +6,7 @@ import reportWebVitals from './reportWebVitals';
 import {BrowserRouter } from 'react-router-dom';
 import ChooseAvatarPage from './pages/ChooseAvatarPage';
 import LandingPage from './pages/LandingPage';
+import TempPage from './pages/TempPage.jsx'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -13,7 +14,8 @@ root.render(
   <BrowserRouter>
     {/*<App />*/}
     {/* <LandingPage /> */}
-    <ChooseAvatarPage/>
+    {/* <ChooseAvatarPage/> */}
+    <TempPage />
   </BrowserRouter>
   </React.StrictMode>
 );
