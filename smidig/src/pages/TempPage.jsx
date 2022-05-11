dc8d63a208ed9091631da2b4e9856439dd20ffcc
Sandra-png <sandra.e@hotmail.no>
2022-05-11 15:30:20 +0200

    Feat: Ny AvatarCreationBubble Component
    Denne komponenten skal brukes i "ProfilePage".

--- smidig/src/pages/TempPage.jsx
+++ smidig/src/pages/TempPage.jsx
@@ -1,12 +1,14 @@
 import React from "react"
 import styled from "styled-components"
 import Badges from "../components/Badges";
+import AvatarCreationBubble from "../components/AvatarCreationBubble";
 
 
 const TempPage = () => {
     return(
         <>
             <Badges />
+            <AvatarCreationBubble />
         </>
     );
 }
