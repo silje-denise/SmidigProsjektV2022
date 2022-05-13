6c6fda2a84a086129a35ce96234948fb38f6d238
annelitangeraas <72303590+annelitangeraas@users.noreply.github.com>
2022-05-13 21:24:14 +0200

    fixed merge errors

--- smidig/src/components/gamesLandingPageComponents/BackgroundGames.jsx
+++ smidig/src/components/gamesLandingPageComponents/BackgroundGames.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 import styled from "styled-components";
-import background from '../../Images/background.png'
+import background from '../../images/background.png';
 
 const StyledBackground = styled.div`
 height: 100px;
 width: 100%;
 z-index: -1;
-background-image: url('../../Images/background.png') ;
+background-image: url('../../images/background.png') ;
 `
 
     
